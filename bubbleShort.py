80d1c3f22801b943a87f89fd849b3b26ec50e5bd
Anthony Heatubun <79141997+Anthony21-DW@users.noreply.github.com>
2022-01-16 06:48:49 +0900

    add feature ascending | descending
    default short is ascending. if we change desc to 'dec', the short will uses descending

--- bubbleShort.py
+++ bubbleShort.py
@@ -1,8 +1,12 @@
-def shortBubble(arr):
+def shortBubble(arr, desc = 'asc'):
   for i in range(len(arr) - 1 ):
     for j in range(len(arr) - i - 1):
-      if arr[j] > arr[j + 1]:
-        arr[j],arr[j+1] = arr[j +1], arr[j]
+      if desc == 'asc':
+        if arr[j] > arr[j + 1]:
+          arr[j],arr[j+1] = arr[j +1], arr[j]
+        elif desc == 'dec':
+          if arr[j] < arr[j + 1]:
+          arr[j],arr[j+1] = arr[j +1], arr[j]
   return arr
 
     
